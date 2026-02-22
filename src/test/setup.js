3a996449d004faf13d0e56c608f84b840b33d3ab
prostak402 <52044373+prostak402@users.noreply.github.com>
2026-02-22 19:40:58 +0400

    Add automated TASK-003 MVP flow coverage

---
+++ src/test/setup.js
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom/vitest';
+
+Object.defineProperty(window.HTMLMediaElement.prototype, 'play', {
+  configurable: true,
+  value: () => Promise.resolve(),
+});
+
+Object.defineProperty(window.HTMLMediaElement.prototype, 'pause', {
+  configurable: true,
+  value: () => {},
+});
+
+Object.defineProperty(window, 'scrollTo', {
+  configurable: true,
+  value: () => {},
+});
+
+if (!window.HTMLElement.prototype.scrollIntoView) {
+  Object.defineProperty(window.HTMLElement.prototype, 'scrollIntoView', {
+    configurable: true,
+    value: () => {},
+  });
+}
